17a8ae285b52d2f036e5e4c511a03c79f74b6a10
hiddehs <hidde@hidde.dev>
2023-06-06 14:11:02 +0200

    serverless telegram bot create

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -4,5 +4,63 @@ import { kv } from '@vercel/kv'
 if (!process.env.TELEGRAM_BOT) {
   throw new Error('Please provide TELEGRAM_TOKEN to continue')
 }
-export const telegramBot = new TelegramBot(process.env.TELEGRAM_BOT,
-  { polling: false })
+
+const createTelegramBot = (token: string) => {
+  console.log("Creating Telegram Bot")
+  const telegramBot = new TelegramBot(token,
+    { polling: false })
+  telegramBot.onText(/\/start/, async (m) => {
+    console.log(`Start messsage from ${m.chat.id}`)
+    await telegramBot.sendMessage(m.chat.id,
+      'Welcome to the Tiny Disco Bot 🚙', {
+        reply_markup: {
+
+          inline_keyboard: [
+            [
+              {
+                text: 'Sub to Donations',
+                callback_data: '/donations',
+              },
+            ],
+          ],
+        },
+      })
+  })
+
+  telegramBot.onText(/\/unsubscribe/, async (m) => {
+    console.log(`Unsubscribing ${m.chat.id}`)
+    await kv.srem('tg.subscription.donations', m.chat.id)
+    return
+  })
+
+  const subscribeToDonations = async (m: TelegramBot.Message | null): Promise<string> => {
+    if (!m) return ''
+    console.log(`Subscribing ${m.chat.id} to donations`)
+    if (await kv.sismember('tg.subscription.donations', m.chat.id)) {
+      return 'Already subscribed'
+    }
+    kv.sadd('tg.subscription.donations', m.chat.id)
+    return 'Subscribed to donations 💸'
+  }
+
+  telegramBot.onText(/\/donations/, async (m) => {
+    await telegramBot.sendMessage(m.chat.id, await subscribeToDonations(m),
+      { parse_mode: 'Markdown' })
+    console.log('donation text handler done')
+  })
+  telegramBot.on('callback_query', async (cb) => {
+    console.log(`[${cb.id}] Callback Query`)
+    if (cb.data === '/donations') await telegramBot.answerCallbackQuery(cb.id,
+      { text: await subscribeToDonations(cb.message ?? null) })
+  })
+
+  telegramBot.on('message', (m) => {
+    console.log(
+      `[${m.message_id}] Message from ${m.chat.first_name} ${m.chat.id}`)
+  })
+
+  return telegramBot
+}
+export const telegramBot = createTelegramBot(process.env.TELEGRAM_BOT)
+
+
