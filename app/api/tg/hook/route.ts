52215fabe2f1e976f46fb18fd1e142a1e17426cb
hiddehs <hidde@hidde.dev>
2023-06-06 13:07:43 +0200

    Force process to keep running ev handlers?

--- app/api/tg/hook/route.ts
+++ app/api/tg/hook/route.ts
@@ -3,5 +3,10 @@ import { telegramBot } from '@/lib/telegram'
 
 export async function POST (request: Request) {
   telegramBot.processUpdate(await request.json())
+
+  await new Promise(resolve => setTimeout(() => {
+    resolve(true)
+  }, 2000))
+
   return new NextResponse('OK')
 }
