7d813ba8a18d97931cfdf84aece9fd7421c23a37
hiddehs <hidde@hidde.dev>
2023-06-17 21:30:30 +0200

    footer navi

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,9 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { NavLink } from '@/lib/types'
+import Link from 'next/link'
 
 interface FooterProps {
   // children: React.ReactNode;
@@ -7,6 +11,15 @@ interface FooterProps {
 
 export const Footer: React.FC<FooterProps> = React.memo(
   (props: FooterProps) => {
+
+    const pathname = usePathname()
+    const links: NavLink[] = [
+      { label: 'Live Updates', href: '/live' },
+      { label: 'Support Us', href: '/support' },
+      { label: 'The Trip', href: '/' },
+      { label: 'Contact Us', href: '/contact' },
+      { label: 'Follow Us', href: '/support' }]
+
     return (
       <footer
         className="space-y-1 flex flex-col w-full bg-midnight-800 pt-9 pb-10 items-center">
@@ -16,26 +29,18 @@ export const Footer: React.FC<FooterProps> = React.memo(
                priority={true}/>
         <div
           className="flex max-w-sm text-center justify-center items-center flex-wrap w-full px-5 py-6">
-          <div
-            className="leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider">
-            LIVE UPDATES
-          </div>
-          <div
-            className="leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider">
-            SUPPORT US
-          </div>
-          <div
-            className="leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider">
-            THE TRIP
-          </div>
-          <div
-            className="leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider">
-            FOLLOW US
-          </div>
-          <div
-            className="leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider">
-            CONTACT US
-          </div>
+
+
+          {links.map(l => {
+            const active = pathname === (l.href)
+            return (<Link href={l.href} key={l.href}
+                          className={`uppercase hover:text-dessert-200 leading-none p-2 whitespace-nowrap text-dessert-500 text-lg text-center tracking-wider ${active
+                            ? 'text-dessert-500'
+                            : 'text-dessert-500/90'}`}>
+              {l.label}
+            </Link>)
+          })}
+
         </div>
       </footer>
     )
