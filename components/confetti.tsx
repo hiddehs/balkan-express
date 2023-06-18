8ae77a5ab07fa905ca85738661724848914e2b53
hiddehs <hidde@hidde.dev>
2023-06-18 16:21:03 +0200

    confetti confetti

--- components/confetti.tsx
+++ components/confetti.tsx
@@ -1,38 +1,16 @@
 'use client'
 import { useEffect } from 'react'
-import ConfettiGenerator from 'confetti-js'
+import JSConfetti from 'js-confetti'
 
 export default async function Confetti () {
 
   useEffect(() => {
-    const confettiSettings = {
-      'target': 'my-canvas',
-      'max': '500',
-      'size': '1',
-      'animate': true,
-      'props': ['circle', 'square', 'triangle', 'line'],
-      'colors': [
-        [165, 104, 246],
-        [230, 61, 135],
-        [0, 199, 228],
-        [253, 214, 126]],
 
-      'clock': '25',
-      'rotate': true,
-      'width': '1972',
-      'height': '1539',
-      'start_from_edge': true,
-      'respawn': true,
-    }
-    const confetti = new ConfettiGenerator(confettiSettings)
-    confetti.render()
+    const jsConfetti = new JSConfetti()
 
-    return () => confetti.clear()
-  }, [])
+    jsConfetti.addConfetti()
 
-  return (
-    <canvas id="my-canvas" className="fixed top-0 h-screen -z-20"/>
-  )
-
+  })
+  return (<div>test</div>)
 }
 
