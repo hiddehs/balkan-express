14aa9be6ca1c6c11f1586eb90766fe53bd2e70a3
hiddehs <hidde@hidde.dev>
2023-06-18 14:44:32 +0200

    thanks page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,16 +1,37 @@
+'use server'
+import { stripe } from '@/lib/stripe'
 import Image from 'next/image'
-import { cookies } from 'next/headers'
+import { Button } from '@/components/button'
+import Confetti from '@/components/confetti'
 
-export default async function Page () {
-  const id = cookies().get('donation')?.value.toString()
+export default async function Page ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const id = searchParams && searchParams['session_id']
+    ? searchParams['session_id']
+    : null
+  if (!id) {
+    return
+  }
+  const session = await stripe.checkout.sessions.retrieve(id)
+
   return (
-    <div className="page flex flex-col items-center justify-between p-24">
-      <Image src="/logo.png" width={215} height={170} alt="Balkan Express"
-             className={'w-[24rem]'}
-             priority={true}/>
+    <div
+      className="page flex z-20 flex-col items-center justify-between p-24 text-center">
+      <Image src="/support-us-header.png" alt="Balkan Express" width={290}
+             height={180}
+             className="mx-auto w-full max-w-sm"
+      />
       <h1
-        className="text-4xl my-12 font-bold uppercase tracking-widest">Thanks {id}!</h1>
-
+        className="text-3xl mt-4 mb-2 font-bold uppercase tracking-widest">Thank you
+        for your donation {session?.customer_email}!</h1>
+      <Button href="https://instagram.com/balkanexpress2023">Follow our
+        journey!</Button>
+      <Confetti/>
     </div>
   )
 }
