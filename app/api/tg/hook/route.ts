84af1ebdba15fc63077f62f7f903741eacf912b8
hiddehs <hidde@hidde.dev>
2023-06-23 13:10:47 +0200

    forever done with vercel cpu event cycles

--- app/api/tg/hook/route.ts
+++ app/api/tg/hook/route.ts
@@ -6,5 +6,11 @@ export const dynamicParams = true;
 
 export async function POST (request: Request) {
   telegramBot.processUpdate(await request.json())
+  // forever done with vercel cpu event cycles
+  await new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve(true)
+    }, 10)
+  })
   return new NextResponse('OK')
 }
