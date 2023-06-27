e83f1b07a9b20b457f360b5bbecb475fc667bda1
hiddehs <hidde@hidde.dev>
2023-06-27 21:13:03 +0200

    Friends cache and logo fixes

--- components/friends.tsx
+++ components/friends.tsx
@@ -1,8 +1,8 @@
-'use server'
 import { supabase } from '@/lib/supabase'
 export const revalidate = 300
 
 export default async function Friends () {
+  'use server'
   const { data: friends } = await supabase.from('friends').
     select()
   const friendsString = friends?.map(f => f.name).join(', ') ?? ''
