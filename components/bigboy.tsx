46e87c6b5caff580223dc3c631fbf39fef077244
hiddehs <hidde@hidde.dev>
2023-07-05 09:56:53 +0200

    breathe

--- components/bigboy.tsx
+++ components/bigboy.tsx
@@ -9,8 +9,8 @@ interface BigboyProps {
 
 export function BigBoy ({ description, logo, className, href }: BigboyProps) {
   return (<a href={href} target={'_blank'}
-             className={`${className} hover:bg-midnight-800/50 duration-100 transition p-4 rounded-md bg-midnight-800 gap-4 flex flex-col text-sm`}>
-    <div className="w-full h-16 flex items-center flex-shrink-0">
+             className={`${className} hover:bg-midnight-800/50 duration-100 transition p-6 rounded-md bg-midnight-800 gap-4 flex flex-col text-sm`}>
+    <div className="w-full h-10 flex items-center flex-shrink-0">
       {logo}
     </div>
     <div className="text-dessert-500 leading-normal">
