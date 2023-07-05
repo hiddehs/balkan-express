f11e41a4e91804ba5697f93323397d2c07578932
Hidde Schultze <hidde.schultze@gmail.com>
2023-07-05 10:32:55 +0200

    Update bigboy.tsx

--- components/bigboy.tsx
+++ components/bigboy.tsx
@@ -10,7 +10,7 @@ interface BigboyProps {
 
 export function BigBoy ({ description, logo, className, href }: BigboyProps) {
   return (<Link href={href} target={'_blank'}
-                className={`${className} hover:bg-midnight-800/50 duration-100 transition p-6 rounded-md bg-midnight-800 gap-4 flex flex-col text-sm`}>
+                className={`${className} hover:bg-midnight-800/50 duration-100 transition p-6 md:rounded-md bg-midnight-800 gap-4 flex flex-col text-sm`}>
     <div className="w-full h-10 flex items-center flex-shrink-0">
       {logo}
     </div>
